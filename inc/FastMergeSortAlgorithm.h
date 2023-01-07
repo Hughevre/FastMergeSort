f9eb46bdfdf2d8483091fef80bd5ee90daf9dbd9
HZ <01121470@pw.edu.pl>
2023-01-07 12:20:26 +0100

    I forgot about some files

--- inc/FastMergeSortAlgorithm.h
+++ inc/FastMergeSortAlgorithm.h
@@ -3,20 +3,22 @@
 
 #include <vector>
 
+/**
+ * The FastMergeSortAlgorithm interface declares operations common to all supported versions
+ * of merge sort algorithm. 
+ */
 class FastMergeSortAlgorithm {
     public:
-        FastMergeSortAlgorithm() = delete;
+        virtual void Sort(std::vector<int>& numbers_array, 
+                          unsigned int      left_index, 
+                          unsigned int      right_index) const = 0;
 
-        static void Sort(std::vector<int>& numbers_array, 
-                         unsigned int      left_index, 
-                         unsigned int      right_index);
-
-    private:
-        static void Merge(std::vector<int>& numbers_array,
-                          unsigned int      left_lower_index, 
-                          unsigned int      left_upper_index, 
-                          unsigned int      right_lower_index, 
-                          unsigned int      right_upper_index);
+    protected:
+        virtual void Merge(std::vector<int>& numbers_array,
+                           unsigned int      left_lower_index, 
+                           unsigned int      left_upper_index, 
+                           unsigned int      right_lower_index, 
+                           unsigned int      right_upper_index) const = 0;
 };
 
 #endif // FAST_MERGE_SORT_ALGORITHM_H
--- tests/IntegersTest.cpp
+++ tests/IntegersTest.cpp
@@ -1,10 +1,12 @@
 #define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_ENABLE_BENCHMARKING
 
+#include <memory>
 #include <algorithm>
 #include <random>
 #include <catch2/catch.hpp>
-#include "../inc/FastMergeSortAlgorithm.h"
+#include "FastMergeSortAlgorithmTestCtx.h"
+#include "../inc/FastMergeSortAlgorithmSeq.h"
 
 TEST_CASE("Nonnegative numbers check", "[main]") {
     /* Create empty vectors to be sorted */
@@ -19,12 +21,15 @@ TEST_CASE("Nonnegative numbers check", "[main]") {
     std::mt19937                    mersenne_engine(rnd_device());
     std::uniform_int_distribution<> u_distribution(u_distribution_lower_bound, u_distribution_upper_bound);
 
+    /* Create test context */
+    FastMergeSortAlgorithmTestCtx test_ctx(std::make_unique<FastMergeSortAlgorithmSeq>());
+
     /* Populate both arrays by some random values */
     std::generate(array_sorted_by_fms.begin(), array_sorted_by_fms.end(), [&]{ return u_distribution(mersenne_engine); });
     array_sorted_by_std = array_sorted_by_fms;
 
     /* Sort the arrays using tho different methods */
-    FastMergeSortAlgorithm::Sort(array_sorted_by_fms, 0, array_sorted_by_fms.size()-1);
+    test_ctx.RunPhase(array_sorted_by_fms, 0, array_sorted_by_fms.size()-1);
     std::sort(array_sorted_by_std.begin(), array_sorted_by_std.end());
 
     SECTION("Correctness check") {
@@ -37,15 +42,11 @@ TEST_CASE("Nonnegative numbers check", "[main]") {
         std::shuffle(array_sorted_by_std.begin(), array_sorted_by_std.end(), mersenne_engine);
 
         BENCHMARK("FastMergeSort implementation") {
-            FastMergeSortAlgorithm::Sort(array_sorted_by_fms, 0, array_sorted_by_fms.size()-1);
+            test_ctx.RunPhase(array_sorted_by_fms, 0, array_sorted_by_fms.size()-1);
         }; // <- SEMICOLON
 
         BENCHMARK("STD library implementation") {
             std::sort(array_sorted_by_std.begin(), array_sorted_by_std.end());
         }; // <- SEMICOLON
     }
-}
-
-TEST_CASE("All integers check", "main") {
-
 }
